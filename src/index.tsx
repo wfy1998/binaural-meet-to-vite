8c322e1c2b58e4f9eae382d76035821994bc648f
wfy1998 <wang.f.ac@m.titech.ac.jp>
2024-01-22 14:16:10 +0900

    change import link

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import {App} from '@/components/App'
-import '@models/audio'  // init audio manager (DO NOT delete)
-import {i18nInit} from '@models/locales'
-import {urlParameters} from '@models/url'
-import {resolveAtEnd} from '@models/utils'
-import errorInfo from '@stores/ErrorInfo'
-import '@stores/index'  // init store (DO NOT delete)
-import contents from '@stores/sharedContents/SharedContents'
+import '@/models/audio'  // init audio manager (DO NOT delete)
+import {i18nInit} from '@/models/locales'
+import {urlParameters} from '@/models/url'
+import {resolveAtEnd} from '@/models/utils'
+import errorInfo from '@/stores/ErrorInfo'
+import '@/stores/index'  // init store (DO NOT delete)
+import contents from '@/stores/sharedContents/SharedContents'
 import {when} from 'mobx'
 import {configure} from "mobx"
 import ReactDOM from 'react-dom'
-import {conference} from '@models/conference'
-import {participants} from '@stores/index'
-import {} from '@models/conference/observeOutputDevice'
+import {conference} from '@/models/conference'
+import {participants} from '@/stores/index'
+import {} from '@/models/conference/observeOutputDevice'
 
 configure({
     enforceActions: "never",
